efc7edab56d1b01e19984783f27fb5c89d103f2e
zach brown <bbrown64506@gmail.com>
2025-06-24 22:30:10 -0500

    refactor: Using NULL over nullptr (convention), avoiding `auto`, fixing `destroy_singly_linked_list` function in singly-linked-list.c

--- singly-linked-list.c
+++ singly-linked-list.c
@@ -9,7 +9,7 @@ static SinglyLinkedNode *create_singly_linked_node(
     SinglyLinkedNode *node = malloc(sizeof(SinglyLinkedNode));
     if (node != NULL) {
         node->data = data;
-        node->next = nullptr;
+        node->next = NULL;
     }
     return node;
 }
@@ -19,14 +19,14 @@ static SinglyLinkedNode *get_index_singly_linked_list(
     const unsigned long long index
 ) {
     if (list->head == NULL || list->size == 0) {
-        return nullptr;
+        return NULL;
     }
 
-    auto current = list->head;
+    SinglyLinkedNode *current = list->head;
     for (unsigned long long i = 0; i < index; i++) {
         current = current->next;
         if (current == NULL) {
-            return nullptr;
+            return NULL;
         }
     }
 
@@ -36,9 +36,9 @@ static SinglyLinkedNode *get_index_singly_linked_list(
 static SinglyLinkedNode *get_tail_singly_linked_list(
     const SinglyLinkedList *list
 ) {
-    auto current = list->head;
+    SinglyLinkedNode *current = list->head;
     if (current == NULL) {
-        return nullptr;
+        return NULL;
     }
 
     while (current->next != NULL) {
@@ -49,12 +49,29 @@ static SinglyLinkedNode *get_tail_singly_linked_list(
 }
 
 SinglyLinkedList *create_singly_linked_list() {
-    return malloc(sizeof(SinglyLinkedList));
+    SinglyLinkedList *list = malloc(sizeof(SinglyLinkedList));
+    if (list != NULL) {
+        list->head = NULL;
+        list->size = 0;
+    }
+    return list;
 }
 
 void destroy_singly_linked_list(
     SinglyLinkedList *list
 ) {
+    SinglyLinkedNode *current = list->head;
+    if (current == NULL) {
+        free(list);
+        return;
+    }
+
+    while (current->next != NULL) {
+        SinglyLinkedNode *temp = current;
+        current = current->next;
+        free(temp);
+    }
+
     free(list);
 }
 
@@ -62,7 +79,7 @@ int insert_at_head_singly_linked_list(
     SinglyLinkedList *list,
     void *data
 ) {
-    const auto new_node = create_singly_linked_node(data);
+    SinglyLinkedNode *new_node = create_singly_linked_node(data);
     if (new_node == NULL) {
         return -1;
     }
@@ -78,12 +95,12 @@ int insert_at_tail_singly_linked_list(
     SinglyLinkedList *list,
     void *data
 ) {
-    const auto new_node = create_singly_linked_node(data);
+    SinglyLinkedNode *new_node = create_singly_linked_node(data);
     if (new_node == NULL) {
         return -1;
     }
 
-    const auto tail = get_tail_singly_linked_list(list);
+    SinglyLinkedNode *tail = get_tail_singly_linked_list(list);
 
     if (tail == NULL) {
         list->head = new_node;
@@ -114,12 +131,12 @@ int insert_at_index_singly_linked_list(
         return insert_at_tail_singly_linked_list(list, data);
     }
 
-    const auto previous_node = get_index_singly_linked_list(list, index - 1);
+    SinglyLinkedNode *previous_node = get_index_singly_linked_list(list, index - 1);
     if (previous_node == NULL) {
         return -1;
     }
 
-    const auto new_node = create_singly_linked_node(data);
+    SinglyLinkedNode *new_node = create_singly_linked_node(data);
     if (new_node == NULL) {
         return -1;
     }
@@ -134,38 +151,38 @@ int insert_at_index_singly_linked_list(
 void *peek_head_singly_linked_list(
     const SinglyLinkedList *list
 ) {
-    return list->head != NULL ? list->head->data : nullptr;
+    return list->head != NULL ? list->head->data : NULL;
 }
 
 void *peek_tail_singly_linked_list(
     const SinglyLinkedList *list
 ) {
-    const auto tail = get_tail_singly_linked_list(list);
+    SinglyLinkedNode *tail = get_tail_singly_linked_list(list);
 
-    return tail != NULL ? tail->data : nullptr;
+    return tail != NULL ? tail->data : NULL;
 }
 
 void *peek_index_singly_linked_list(
     const SinglyLinkedList *list,
     const unsigned long long index
 ) {
-    const auto tail = get_index_singly_linked_list(list, index);
+    const SinglyLinkedNode *tail = get_index_singly_linked_list(list, index);
 
-    return tail != NULL ? tail->data : nullptr;
+    return tail != NULL ? tail->data : NULL;
 }
 
 void *remove_head_singly_linked_list(
     SinglyLinkedList *list
 ) {
     if (list->head == NULL) {
-        return nullptr;
+        return NULL;
     }
 
-    const auto node = list->head;
+    SinglyLinkedNode *node = list->head;
     list->head = list->head->next;
     list->size--;
 
-    const auto data = node->data;
+    SinglyLinkedNode *data = node->data;
     free(node);
 
     return data;
@@ -174,17 +191,17 @@ void *remove_head_singly_linked_list(
 void *remove_tail_singly_linked_list(
     SinglyLinkedList *list
 ) {
-    const auto before_tail = get_index_singly_linked_list(list, list->size - 2);
+    SinglyLinkedNode *before_tail = get_index_singly_linked_list(list, list->size - 2);
 
     if (before_tail == NULL) {
-        return nullptr;
+        return NULL;
     }
 
-    const auto tail = before_tail->next;
-    before_tail->next = nullptr;
+    SinglyLinkedNode *tail = before_tail->next;
+    before_tail->next = NULL;
     list->size--;
 
-    const auto data = tail->data;
+    SinglyLinkedNode *data = tail->data;
     free(tail);
 
     return data;
@@ -194,17 +211,17 @@ void *remove_index_singly_linked_list(
     SinglyLinkedList *list,
     const unsigned long long index
 ) {
-    const auto before_node = get_index_singly_linked_list(list, index - 1);
+    SinglyLinkedNode *before_node = get_index_singly_linked_list(list, index - 1);
 
     if (before_node == NULL) {
-        return nullptr;
+        return NULL;
     }
 
-    const auto node = before_node->next;
+    SinglyLinkedNode *node = before_node->next;
     before_node->next = before_node->next->next;
     list->size--;
 
-    const auto data = node->data;
+    SinglyLinkedNode *data = node->data;
     free(node);
 
     return data;
@@ -214,7 +231,7 @@ void print_singly_linked_list(
     const SinglyLinkedList *list,
     void (*print)(void *data)
 ) {
-    auto current = list->head;
+    SinglyLinkedNode *current = list->head;
     if (current != NULL) {
         while (current->next != NULL) {
             print(current->data);
